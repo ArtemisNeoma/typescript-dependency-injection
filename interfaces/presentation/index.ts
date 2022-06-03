385c3f9b97376db6bfdeb0d72363c45548a492f9
Neo-WRN <nicolas.destito@warren.com.br>
2022-06-03 15:14:21 -0300

    refactor: added IControllerUser and type RouteBase<Type>

--- interfaces/presentation/index.ts
+++ interfaces/presentation/index.ts
@@ -1,8 +1,15 @@
 import { NextFunction, Request, Response, Router } from 'express'
 import { IEndPointsService } from 'interfaces/domain'
 
+export type RouteBase<Type> = (req: Request, res: Response, next?: NextFunction) => Promise<Type>
+
 export interface IEndPointsController {
   service: IEndPointsService
+}
+
+export interface IControllerUser extends IEndPointsController {
+  createUser: RouteBase<Response>
+  listUsers: RouteBase<void>
 }
 
 export type RouteMiddleware = (req: Request, res: Response, next: NextFunction) => Promise<void>
