5f6f37d02aa7015ab8f40d6e9b74216116576f42
Neo-WRN <nicolas.destito@warren.com.br>
2022-06-03 12:08:59 -0300

    feat: created abstract EndPointsRouter class

---
+++ presentation/routes/AbstractRouter.ts
@@ -0,0 +1,16 @@
+import { Router } from 'express'
+import { IEndPointsController, IEndPointsRouter, RouteMiddleware } from 'interfaces/presentation'
+
+export default abstract class EndPointsRouter implements IEndPointsRouter {
+  router: Router
+  controller: IEndPointsController
+  middlewares: {[index: string]: RouteMiddleware}
+
+  constructor (router: Router, controller: IEndPointsController, middlewares: {[index: string]: RouteMiddleware}) {
+    this.router = router
+    this.controller = controller
+    this.middlewares = middlewares
+  }
+
+  abstract routes (): Promise<void>
+}
