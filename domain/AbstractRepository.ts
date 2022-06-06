465d1ffc2acadbc3483ec25338032bbc3cca2992
Neo-WRN <nicolas.destito@warren.com.br>
2022-06-06 17:30:36 -0300

    feat: created AbstractRepository

---
+++ domain/AbstractRepository.ts
@@ -0,0 +1,15 @@
+import { IEndPointsRepository, IRepositoryDatabase } from 'interfaces/domain'
+
+export default abstract class AbstractRepository implements IEndPointsRepository {
+  database: IRepositoryDatabase
+
+  constructor (database: IRepositoryDatabase) {
+    this.database = database
+  }
+
+  abstract create (entity: object): Promise<void>
+  abstract read: (id: number) => Promise<void | object>
+  abstract readAll: () => Promise<void | object>
+  abstract update: (id: number, newEntity: object) => Promise<void>
+  abstract delete: (id: number) => Promise<void>
+}
