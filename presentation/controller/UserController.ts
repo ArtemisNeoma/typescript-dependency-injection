1b48f9a5cd422a04e9ab26781038e8b27a9f2c2f
Neo-WRN <nicolas.destito@warren.com.br>
2022-06-06 15:27:36 -0300

    refactor: added createUser code to UserController

--- presentation/controller/UserController.ts
+++ presentation/controller/UserController.ts
@@ -1,10 +1,18 @@
 import { Request, Response } from 'express'
+import { IServiceUser } from 'interfaces/domain'
 import { IControllerUser } from 'interfaces/presentation'
 import AbstractController from './AbstractController'
 
 export default class UserController extends AbstractController implements IControllerUser {
+  service: IServiceUser
+  constructor (service: IServiceUser) {
+    super(service)
+    this.service = service
+  }
+
   async createUser (req: Request, res: Response): Promise<Response> {
-    return res
+    const serviceResponse = await this.service.create(req.body)
+    return res.status(serviceResponse.code).json(serviceResponse.info)
   }
 
   async listUsers (req: Request, res: Response): Promise<void> {
