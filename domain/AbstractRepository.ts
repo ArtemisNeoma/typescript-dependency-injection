e886bb8956854920e19400a484ac7e5297a0291c
Neo-WRN <nicolas.destito@warren.com.br>
2022-06-06 17:36:29 -0300

    bug: fixed wrong abstract method declaration at AbstractRepository

--- domain/AbstractRepository.ts
+++ domain/AbstractRepository.ts
@@ -8,8 +8,8 @@ export default abstract class AbstractRepository implements IEndPointsRepository
   }
 
   abstract create (entity: object): Promise<void>
-  abstract read: (id: number) => Promise<void | object>
-  abstract readAll: () => Promise<void | object>
-  abstract update: (id: number, newEntity: object) => Promise<void>
-  abstract delete: (id: number) => Promise<void>
+  abstract read (id: number): Promise<void | object>
+  abstract readAll (): Promise<void | object>
+  abstract update (id: number, newEntity: object): Promise<void>
+  abstract delete (id: number): Promise<void>
 }
