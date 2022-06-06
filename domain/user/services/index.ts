e74d8b5498231b8d2bb82ff6598bc7d1ef27c805
Neo-WRN <nicolas.destito@warren.com.br>
2022-06-06 17:04:59 -0300

    feat: created UserService

---
+++ domain/user/services/index.ts
@@ -0,0 +1,24 @@
+import AbstractService from '@domain/AbstractService'
+import { Request } from 'express'
+import { IEndPointsRepository, IServiceResponse, IServiceUser, IServiceValidationGroup } from 'interfaces/domain'
+
+export default class UserService extends AbstractService implements IServiceUser {
+  createSchema: IServiceValidationGroup
+  constructor (repository: IEndPointsRepository, createSchema: IServiceValidationGroup) {
+    super(repository)
+    this.createSchema = createSchema
+  }
+
+  async create (user: Request): Promise<IServiceResponse> {
+    try {
+      const { schema, context } = this.createSchema
+      const newUser = await schema.validateAsync(user, context)
+      this.repository.create(newUser)
+      return { code: 201, info: 'User Created' }
+    } catch (err: any) {
+      return { code: 422, info: err }
+    }
+  }
+
+  async readAll (): Promise<IServiceResponse> {}
+}
