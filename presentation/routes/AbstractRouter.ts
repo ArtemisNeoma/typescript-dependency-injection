00838d341eeb8ad88daf24e4cde484950d9bd149
Neo-WRN <nicolas.destito@warren.com.br>
2022-06-08 18:56:06 -0300

    refactor: separated controllers and middleware from AbstractRouter

--- presentation/routes/AbstractRouter.ts
+++ presentation/routes/AbstractRouter.ts
@@ -1,15 +1,11 @@
 import { Router } from 'express'
-import { IEndPointsController, IEndPointsRouter, RouteMiddleware } from 'interfaces/presentation'
+import { IEndPointsRouter } from 'interfaces/presentation'
 
 export default abstract class AbstractRouter implements IEndPointsRouter {
   router: Router
-  controller: IEndPointsController
-  middlewares: {[index: string]: RouteMiddleware}
 
-  constructor (router: Router, controller: IEndPointsController, middlewares: {[index: string]: RouteMiddleware} = {}) {
+  constructor (router: Router) {
     this.router = router
-    this.controller = controller
-    this.middlewares = middlewares
   }
 
   abstract routes (): Promise<void>
