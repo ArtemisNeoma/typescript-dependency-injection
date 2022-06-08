42986d20d4986e97e3ebaea6c2b5f8df8bc5eb7c
Neo-WRN <nicolas.destito@warren.com.br>
2022-06-08 12:36:41 -0300

    refactor: added app.listen and small style corrections to www.ts

--- bin/www.ts
+++ bin/www.ts
@@ -1,7 +1,11 @@
-import { config } from "dotenv";
-import { onListening, onError } from "./serverInfo";
+/* eslint-disable no-undef */
+import { config } from 'dotenv'
+import { onListening, onError } from './serverInfo'
 import app from 'index'
 
 config({ path: '/config/config.env' })
 const port = process.env.PORT || 3000
 
+app.listen(port, () => {})
+  .on('error', (error: NodeJS.ErrnoException) => onError(error, port))
+  .on('listening', () => onListening(port))
