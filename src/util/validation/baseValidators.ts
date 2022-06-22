16b11980a8395d66b2ef152bf37393dbf135b5ed
Neo-WRN <nicolas.destito@warren.com.br>
2022-06-22 17:01:05 -0300

    refactor: added missing return types on util baseValidators

--- src/util/validation/baseValidators.ts
+++ src/util/validation/baseValidators.ts
@@ -1,38 +1,38 @@
 import { IDateValidationOptions, IStringValidationOptions } from '@interfaces/util'
-import Joi from 'joi'
+import Joi, { BooleanSchema, DateSchema, StringSchema } from 'joi'
 import sanitizeHtml from 'sanitize-html'
 
 const { string, date, boolean } = Joi.types()
-const escapeHtmlString = (value: string, helpers: Joi.CustomHelpers) => {
+const escapeHtmlString = (value: string, helpers: Joi.CustomHelpers): string => {
   if (typeof value !== 'string') helpers.error('string.base')
   return sanitizeHtml(value)
 }
-export const stringLenghtValidation = (schema: Joi.StringSchema, min?: number, max?: number) => {
+export const stringLenghtValidation = (schema: Joi.StringSchema, min?: number, max?: number): StringSchema => {
   const minSchema = (typeof min === 'number') ? schema.min(min) : schema
   return (typeof max === 'number') ? minSchema.max(max) : minSchema
 }
 
-export const dateLenghtValidation = (schema: Joi.DateSchema, min?: string | Date, max?: string | Date) => {
+export const dateLenghtValidation = (schema: Joi.DateSchema, min?: string | Date, max?: string | Date): DateSchema => {
   const lessSchema = (typeof min === 'number') ? schema.less(min) : schema
   return (typeof max === 'number') ? lessSchema.greater(max) : lessSchema
 }
 
-export const stringValidation = ({ name, min, max }: IStringValidationOptions) => {
+export const stringValidation = ({ name, min, max }: IStringValidationOptions): StringSchema => {
   const baseSchema = string
     .trim()
     .custom(escapeHtmlString, 'Sanitizes HTML code included in the string for safety')
   return stringLenghtValidation(baseSchema, min, max)
 }
 
-export const numberStringValidation = ({ name, min, max }: IStringValidationOptions) => {
+export const numberStringValidation = ({ name, min, max }: IStringValidationOptions): StringSchema => {
   const baseSchema = stringValidation({ name, min, max })
     .custom((value: string) => { console.log(value); return value })
     .replace(/\D/g, '')
   return stringLenghtValidation(baseSchema, min, min)
 }
 
-export const dateValidation = ({ name, min, max }: IDateValidationOptions) => {
+export const dateValidation = ({ name, min, max }: IDateValidationOptions): DateSchema => {
   const baseSchema = date.iso()
   return dateLenghtValidation(baseSchema, min, max)
 }
-export const booleanValidation = () => boolean
+export const booleanValidation = (): BooleanSchema => boolean
