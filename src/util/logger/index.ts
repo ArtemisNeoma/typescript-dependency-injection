d20bef680b59e7c2fff6c6e4493c68df8feb2fb9
Neo-WRN <nicolas.destito@warren.com.br>
2022-06-22 16:58:08 -0300

    refactor: added missing return types on util/logger index

--- src/util/logger/index.ts
+++ src/util/logger/index.ts
@@ -1,7 +1,7 @@
-import { createLogger, format, transports } from 'winston'
+import { createLogger, format, Logform, transports } from 'winston'
 
 const baseFormat = format.printf(
-  ({ level, message, label, timestamp }) =>
+  ({ level, message, label, timestamp }: Logform.TransformableInfo) =>
     `[${timestamp} - ${label}] ${level}: ${message}`)
 
 export const serverLogger = createLogger({
