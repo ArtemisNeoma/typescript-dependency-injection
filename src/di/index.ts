af47d32fd3b13f448a8fb6304a59a3de5021441f
Neo-WRN <nicolas.destito@warren.com.br>
2022-06-23 17:42:21 -0300

    style: added section comments for di index.ts

--- src/di/index.ts
+++ src/di/index.ts
@@ -1,19 +1,22 @@
 import UserRepository from '@domain/user/repository/UserRepository'
-import UserService from '@domain/user/services'
+import CreateUserService from '@domain/user/services/CreateUserService'
 import { userCreateValidation } from '@domain/user/services/helpers/userValidation'
+import ListUserService from '@domain/user/services/ListUserService'
 import DatabaseVariable from 'database/variable'
 import { Router } from 'express'
-import { IRepositoryDatabase, IRepositoryUser, IServiceUser, IServiceValidationGroup } from 'interfaces/domain'
+import { IRepositoryDatabase, IRepositoryUser, IServiceCreateUser, IServiceListUser, IServiceValidationGroup } from 'interfaces/domain'
 import { ICreateUserController, IListUserController } from 'interfaces/presentation'
 import CreateUserController from 'presentation/controller/CreateUserController'
 import ListUserController from 'presentation/controller/ListUserController'
 import { container } from 'tsyringe'
 
+// Repositories
 container.registerSingleton<IRepositoryDatabase>(
   'Database',
   DatabaseVariable
 )
 
+// Services
 container.registerSingleton<IRepositoryUser>(
   'UserRepository',
   UserRepository
@@ -28,6 +31,7 @@ container.register<IServiceUser>(
   UserService
 )
 
+// Routers
 container.register<Router>(
   'FrameworkRouter',
   { useValue: Router() }
