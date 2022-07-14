90f8977a4edbb3616086b270732c6595142237f2
Neo-WRN <nicolas.destito@warren.com.br>
2022-07-14 13:15:57 -0300

    refactor: added throw error and void return to getCep

--- src/domain/user/services/helpers/getCep.ts
+++ src/domain/user/services/helpers/getCep.ts
@@ -1,12 +1,12 @@
 import axios from 'axios'
 
-const getCep = async (value: string): Promise<object> => {
+const getCep = async (value: string): Promise<void> => {
   try {
     const { status } = await axios.get(`https://cep.awesomeapi.com.br/json/${value}`)
-    if (status === 200) return { result: true }
-    return { result: true, msg: 'postal_code is not valid' }
+    if (status === 200) return
+    throw new Error('postal_code is invalid')
   } catch (error) {
-    return { result: false, msg: 'Error on validating postal_code' }
+    throw new Error('CEP API failed to validate')
   }
 }
 
