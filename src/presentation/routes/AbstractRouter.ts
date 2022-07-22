63750b97397b801f7587e8acdcc543470ec8670b
Neo-WRN <nicolas.destito@warren.com.br>
2022-07-22 11:36:00 -0300

    refactor: changed routes property to () => void at Abstract Router

--- src/presentation/routes/AbstractRouter.ts
+++ src/presentation/routes/AbstractRouter.ts
@@ -14,7 +14,7 @@ export default abstract class AbstractRouter implements IEndPointsRouter {
     return this._routes;
   }
 
-  set routes(method: () => Promise<void>) {
+  set routes(method: () => void) {
     this._routes = method;
   }
 }
