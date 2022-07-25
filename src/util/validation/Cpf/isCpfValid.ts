0c53805913f71a8ec4344e3e1a26355d443da52c
Neo-WRN <nicolas.destito@warren.com.br>
2022-07-25 15:37:51 -0300

    refactor: removed let from isCpfValid

--- src/util/validation/Cpf/isCpfValid.ts
+++ src/util/validation/Cpf/isCpfValid.ts
@@ -2,7 +2,7 @@ import StatusError from '@util/error';
 import calcCpfDigit from './calcCpfDigit';
 
 const isCpfValid = (cpf: string): void => {
-  let cpfArray = Array.from(cpf, Number);
+  const cpfArray = Array.from(cpf, Number);
   for (let i = 1; i < cpfArray.length; i++) {
     if (cpfArray[i] !== cpfArray[0]) {
       break;
@@ -11,13 +11,13 @@ const isCpfValid = (cpf: string): void => {
     }
   }
   const confirmationDigits = cpfArray.slice(-2);
-  cpfArray = cpfArray.slice(0, -2);
-  const firstDigit = calcCpfDigit(cpfArray);
+  const slicedCpfArray = cpfArray.slice(0, -2);
+  const firstDigit = calcCpfDigit(slicedCpfArray);
   if (firstDigit !== confirmationDigits[0]) {
     throw new StatusError(422, 'cpf is not valid');
   }
-  cpfArray.push(firstDigit);
-  if (calcCpfDigit(cpfArray, 0) !== confirmationDigits[1]) {
+  slicedCpfArray.push(firstDigit);
+  if (calcCpfDigit(slicedCpfArray, 0) !== confirmationDigits[1]) {
     throw new StatusError(422, 'ValidationError: cpf is not valid');
   }
 };
